68ec17f3657fdbb3d23c8bd12c71b2d7adbfcbc1
premnath <premdevapp@gmail.com>
2020-11-29 19:25:55 +0530

    main

--- app/src/main/java/com/example/nodoapp/MainActivity.java
+++ app/src/main/java/com/example/nodoapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.nodoapp;
 
 import android.os.Bundle;
 
+import com.example.nodoapp.ui.NoDoListAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
@@ -15,12 +18,20 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NoDoListAdapter noDoListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        noDoListAdapter = new NoDoListAdapter(this);
+        recyclerView.setAdapter(noDoListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
